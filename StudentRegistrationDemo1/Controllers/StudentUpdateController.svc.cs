8f0d7a811fada7b225bb06a3f6037e76b8b8ba10
prateekparallel <prateekparallel@gmail.com>
2018-08-18 21:02:21 +0100

    Added Update Controller class file

---
+++ StudentRegistrationDemo1/Controllers/StudentUpdateController.svc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+using StudentRegistrationDemo1.Dataobject;
+
+namespace StudentRegistrationDemo1.Controllers
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class StudentUpdateController
+    {
+        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+        // To create an operation that returns XML,
+        //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
+        //     and include the following line in the operation body:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        [OperationContract]
+        public void DoWork()
+        {
+            // Add your operation implementation here
+            return;
+        }
+
+        // Add more operations here and mark them with [OperationContract][OperationContract]
+        [WebInvoke(Method = "PUT",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "/Update.json")]
+        public String updateStudent(Student studentregd)
+        {
+            try
+            {
+                return StudentRegistration.getInstance().UpdateStudent(studentregd);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+
+    }
+}
